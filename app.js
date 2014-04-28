5e78e1efb01703842c98ea511900016a6bb774c8
Pat Sanchez <splatwes@gmail.com>
2014-04-28 09:58:26 -0500

    Removed variables from global scope

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+//Libraries we will use
 var express = require("express");
 var exphbs  = require('express3-handlebars');
 
+//App Setup
 var app = express();
 var port = 3700;
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+//Defining our Routes
 app.get("/", function(req, res){
     res.render('index');
 });
@@ -14,10 +17,16 @@ app.get("/", function(req, res){
 //Sets the static directory for files that will be rendered to the client as is
 app.use(express.static(__dirname + '/public'));
 
+//Starts running the app using socket.io
 var io = require('socket.io').listen(app.listen(port));
 
+//For each connection
 io.sockets.on('connection', function (socket) {
+
+    //Send them a message welcoming them to the chat
     socket.emit('message', { message: 'Welcome to the chat. Play nice!', username: 'Server' });
+
+    //And each time you receive a send, emit it to everyone
     socket.on('send', function (data) {
         io.sockets.emit('message', data);
     });
--- public/chat.js
+++ public/chat.js
@@ -1,52 +1,56 @@
 window.onload = function() {
-    var messages = [],
-    socket = io.connect('http://localhost:3700'),
-    field = document.getElementById("field"),
-    sendButton = document.getElementById("send"),
-    content = document.getElementById("content"),
-    name = document.getElementById("name"),
-    sendMessage;
+    var chat = new(function() {
+        this.messages = [];
 
-    socket.on('message', function (data) {
-        if(data.message) {
-            messages.push(data);
+        this.socket = io.connect('http://localhost:3700');
 
-            //Adds new message to array of all messages and then display to the user
-            var html = '';
-            for(var i=0; i<messages.length; i++) {
-                html += '<b>' + messages[i].username + ': </b>';
-                html += messages[i].message + '<br />';
+        this.userInputField = document.getElementById("userInput");
+
+        this.content = document.getElementById("content");
+
+        this.nameField = document.getElementById("name");
+
+        this.sendButton = document.getElementById("send");
+
+        this.socket.on('message', function (data) {
+            if(data.message) {
+                this.messages.push(data);
+
+                //Adds new message to array of all messages and then display to the user
+                var html = '';
+                for(var i=0; i < this.messages.length; i++) {
+                    html += '<b>' + this.messages[i].username + ': </b>';
+                    html += this.messages[i].message + '<br />';
+                }
+                this.content.innerHTML = html;
+
+                //Then scroll the user to the bottom of the content div
+                this.content.scrollTop = this.content.scrollHeight;
+            } else {
+                //Generic error handling if the server responded without a message
+                console.log("There is a problem:", data);
             }
-            content.innerHTML = html;
+        }.bind(this));
 
-            //Then scroll the user to the bottom of the content div
-            content.scrollTop = content.scrollHeight;
-        } else {
-            //Generic error handling if the server responded without a message
-            console.log("There is a problem:", data);
-        }
+        this.sendButton.onclick = this.sendMessage = function() {
+            if(this.nameField.value === '') {
+                alert("Please type your name!");
+            } else if (this.userInputField.value === '') {
+                alert("Place enter some text!");
+            } else {
+                //Send an object with a message and username property to the server
+                this.socket.emit('send', { message: this.userInputField.value, username: this.nameField.value });
+
+                //Clear out the user's inputs and refocus them back on the input
+                this.userInputField.value = "";
+                this.userInputField.focus();
+            }
+        }.bind(this);
+
+        this.userInputField.onkeyup = function(e){
+            if(e.keyCode === 13){
+                this.sendMessage();
+            }
+        }.bind(this);
     });
-
-    sendButton.onclick = sendMessage = function() {
-        //Don't let them submit unless they have given us a name and some text
-        if(name.value === '') {
-            alert("Please type your name!");
-        } else if (field.value === '') {
-            alert("Place enter some text!");
-        } else {
-            //Send an object with a message and username property to the server
-            socket.emit('send', { message: field.value, username: name.value });
-
-            //Clear out the user's inputs and refocus them back on the input
-            field.value = "";
-            field.focus();
-        }
-    };
-
-    field.onkeyup = function(e){
-        if(e.keyCode === 13){
-            sendMessage();
-        }
-    }
-
 };
